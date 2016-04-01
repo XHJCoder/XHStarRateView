56ee0ed81c89e64113bc9e2f84097b482af328a9
huahua0809 <1149949564@qq.com>
2016-04-01 10:39:20 +0800

    Initial Commit

---
+++ XHStarRateView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHStarRateView
+//
+//  Created by 江欣华 on 16/4/1.
+//  Copyright © 2016年 jxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XHStarRateView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHStarRateView
+//
+//  Created by 江欣华 on 16/4/1.
+//  Copyright © 2016年 jxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
