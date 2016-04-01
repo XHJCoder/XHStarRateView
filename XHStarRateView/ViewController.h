3cde16f951ba8dd540b102c3a197b931c57c47e9
huahua0809 <1149949564@qq.com>
2016-04-01 16:44:41 +0800

    星星评论

--- XHStarRateView/ViewController.h
+++ XHStarRateView/ViewController.h
@@ -2,7 +2,7 @@
 //  ViewController.h
 //  XHStarRateView
 //
-//  Created by 江欣华 on 16/4/1.
+//  Created by huahua on 16/4/1.
 //  Copyright © 2016年 jxh. All rights reserved.
 //
 
--- XHStarRateView/ViewController.m
+++ XHStarRateView/ViewController.m
@@ -2,13 +2,14 @@
 //  ViewController.m
 //  XHStarRateView
 //
-//  Created by 江欣华 on 16/4/1.
+//  Created by huahua on 16/4/1.
 //  Copyright © 2016年 jxh. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "XHStarRateView.h"
 
-@interface ViewController ()
+@interface ViewController () <XHStarRateViewDelegate>
 
 @end
 
@@ -16,12 +17,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    XHStarRateView *starRateView = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 60, 200, 30)];
+    starRateView.isAnimation = YES;
+    starRateView.rateStyle = IncompleteStar;
+    starRateView.tag = 1;
+    starRateView.delegate = self;
+    [self.view addSubview:starRateView];
+    
+    XHStarRateView *starRateView2 = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 100, 200, 30) numberOfStars:5 rateStyle:HalfStar isAnination:YES delegate:self];
+    starRateView2.tag = 2;
+    [self.view addSubview:starRateView2];
+    
+    XHStarRateView *starRateView3 = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 140, 200, 30) finish:^(CGFloat currentScore) {
+        NSLog(@"3----  %f",currentScore);
+    }];
+    [self.view addSubview:starRateView3];
+    
+    XHStarRateView *starRateView4 = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 180, 200, 30) numberOfStars:5 rateStyle:HalfStar isAnination:YES finish:^(CGFloat currentScore) {
+        NSLog(@"4----  %f",currentScore);
+    }];
+    [self.view addSubview:starRateView4];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)starRateView:(XHStarRateView *)starRateView currentScore:(CGFloat)currentScore{
+    NSLog(@"%ld----  %f",starRateView.tag,currentScore);
 }
 
 @end
