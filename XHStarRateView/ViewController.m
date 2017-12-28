c0b8d80038f5a209bb3915f80b4dc96d12cab461
Qilin Hu <523013205@qq.com>
2017-12-28 16:47:46 +0800

    规范、统一项目中相关属性及方法的命名方式。
    本次提交并没有改变相关功能的实现原理，只是使项目使用的 Objective-C 代码更清晰，并尽量使其遵循规范。
    
    修改内容大致如下：
    - 重命名相关属性、方法，命名习惯及原则尽量参考了官方开发文档的命名方式。
    - 为 Block、枚举 类型增加了 XHStarRateView 前缀。
    - 为 XHStarRateView 类设置指定初始化方法，其他初始化方法均调用指定初始化方法以维持调用链。参考《Effective Objective-C》第16条。
    - 用类型常量代替 #define 预处理指令。参考: https://github.com/oa414/objc-zen-book-cn#常量  或参考《Effective Objective-C》第4条。
    - userTapRateView: 方法中取消了 default 分支，当在 switch 语句里面使用一个可枚举的变量的时候，default 是不必要的。参考: https://github.com/oa414/objc-zen-book-cn#case语句
    - #pragma mark 参考了 https://github.com/oa414/objc-zen-book-cn#pragma

--- XHStarRateView/ViewController.m
+++ XHStarRateView/ViewController.m
@@ -17,31 +17,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    /*
+     1. Delegate 方式创建
+     */
     XHStarRateView *starRateView = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 60, 200, 30)];
-    starRateView.isAnimation = YES;
-    starRateView.rateStyle = IncompleteStar;
+    starRateView.isAnimation = YES; // 有动画
+    starRateView.rateStyle = XHStarRateViewRateStyeIncompleteStar; //允许不完整星评论
     starRateView.tag = 1;
     starRateView.delegate = self;
     [self.view addSubview:starRateView];
     
-    XHStarRateView *starRateView2 = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 100, 200, 30) numberOfStars:5 rateStyle:HalfStar isAnination:YES delegate:self];
+    /*
+     2. 初始化方法创建
+     半星评论、无动画
+     */
+    XHStarRateView *starRateView2 = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 100, 200, 30)
+                                                             numberOfStar:5
+                                                                rateStyle:XHStarRateViewRateStyeHalfStar
+                                                              isAnimation:NO
+                                                                 delegate:self];
     starRateView2.tag = 2;
     [self.view addSubview:starRateView2];
     
-    XHStarRateView *starRateView3 = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 140, 200, 30) finish:^(CGFloat currentScore) {
+    /*
+     3. block 方法1
+     默认设置：完整星评论、
+     */
+    XHStarRateView *starRateView3 = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 140, 200, 30) completion:^(CGFloat currentScore) {
         NSLog(@"3----  %f",currentScore);
     }];
+    
     [self.view addSubview:starRateView3];
     
-    XHStarRateView *starRateView4 = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 180, 200, 30) numberOfStars:5 rateStyle:HalfStar isAnination:YES finish:^(CGFloat currentScore) {
+    /*
+     4. block 方法2
+     半星评论、有动画
+     */
+    XHStarRateView *starRateView4 = [[XHStarRateView alloc] initWithFrame:CGRectMake(20, 180, 200, 30) numberOfStar:8 rateStyle:XHStarRateViewRateStyeHalfStar isAnimation:YES completion:^(CGFloat currentScore) {
         NSLog(@"4----  %f",currentScore);
     }];
     [self.view addSubview:starRateView4];
     
 }
 
--(void)starRateView:(XHStarRateView *)starRateView currentScore:(CGFloat)currentScore{
-    NSLog(@"%ld----  %f",starRateView.tag,currentScore);
+-(void)starRateView:(XHStarRateView *)starRateView ratingDidChange:(CGFloat)currentRating {
+    NSLog(@"%ld----  %f",(long)starRateView.tag,currentRating);
 }
 
 @end
